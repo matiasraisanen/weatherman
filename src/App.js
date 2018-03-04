d28f8f5f30aefb51659d47d8435e37ba1012b4f1
matiasraisanen <33057754+matiasraisanen@users.noreply.github.com>
2018-03-04 10:52:50 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+/*
+ Weather Man, by Matias Räisänen 2018
+  A ReactJS class for fetching weather data of a city of the user's choice.
+  Ability to save searches into the user interface, and to delete them.
+  Application logo by Jero FX @ behance.net: https://www.behance.net/gallery/30971807/Animated-weather-icons-(gif)
+*/
+
 import React, { Component } from 'react';
-import logo from './sunny.gif'; // Logo by JeroFX @ behance.net : https://www.behance.net/gallery/30971807/Animated-weather-icons-(gif)
+import logo from './sunny.gif';
 import './App.css';
 
 class App extends Component {
@@ -10,23 +17,21 @@ class App extends Component {
   }
 
   getWeather = () => {
-    //Function for fetching the weather data
-    if (this.state.userInput === '') {   //If input is left empty, show an alert  
+    //Fetch the weather data
+    if (this.state.userInput === '') {   //If input is left empty, show an alert
       alert("Error: city cannot be empty!")
-    } else {
+    }else{  //Else proceed with fetching the weather daata
      fetch('http://api.openweathermap.org/data/2.5/weather?q=' + this.state.userInput + '&appid=7c4e12c423a54c4abaaaae8a0b1e0062&units=metric')
           .then((response) => response.json()) //Fetch the JSON response
           .then((responseData) => {
-            // eslint-disable-next-line
-            if (responseData.cod == '404'){ //If no city is found with the given name, show an alert
+            if (responseData.cod === '404'){ //If no city is found with the given name, show an alert
               alert("Error: city not found")
-            } else {  //Else add the city's data into state.
-              this.setState({city: this.state.userInput});
+            }else{  //Else add the city's data into state.
               this.setState({
-                city: responseData.name, 
-                temperature: responseData.main.temp,
+                city: responseData.name,
+                temperature: parseInt(responseData.main.temp), //No decimals in temperature
                 weather: responseData.weather[0].main,
-                icon: responseData.weather[0].icon 
+                icon: "http://openweathermap.org/img/w/" + responseData.weather[0].icon + ".png", //Construct the imageURL using the icon name.
               });
             }
           });
@@ -34,38 +39,36 @@ class App extends Component {
   }
 
   inputChanged = (event) => {
-  this.setState({userInput: event.target.value});
+    //Change the state of userInput when the user types into text box.
+    this.setState({userInput: event.target.value});
   }
 
   saveCity = () => {
-  //Function for saving the city into user interface
+  //Save the city into user interface
     if (this.state.city === '') { //If no city is selected, show an alert
       alert("Error: no city selected!")
-    } else {
-    var newCity = {city: this.state.city, weather: this.state.weather, temperature: this.state.temperature, icon: "http://openweathermap.org/img/w/" + this.state.icon + ".png"};
-    this.setState({savedCities: [...this.state.savedCities, newCity]}); //Add newCity to the list of saved cities.
+    }else{
+      var newCity = {city: this.state.city, weather: this.state.weather, temperature: this.state.temperature, icon: this.state.icon};
+      this.setState({savedCities: [...this.state.savedCities, newCity]}); //Add newCity to the list of saved cities.
     }
   }
 
   clear = () => {
-  //Function for clearing all saved cities.
+  //Clear all saved cities.
     this.setState({savedCities: []});
   }
 
   render() {
 
-    const itemRows = this.state.savedCities.map((city) =>
-     //Extract data from the list of saved cities, and present each in its own div 
-      <div className="oneSaved" key={city.city}>
-        {city.city}<br />
-        {city.temperature}°C<br />
-        {city.weather}<br />
-        <img src={city.icon} alt='icon'></img><br />
+    const cityDivs = this.state.savedCities.map((city, index) =>
+    //Extract data from the list of saved cities, and present each in its own division
+      <div className="oneSaved" key={index}>
+        {city.city}<br/>
+        {city.temperature}°C<br/>
+        {city.weather}<br/>
+        <img src={city.icon} alt='icon'></img><br/>
       </div>
     )
-
-    const url = "http://openweathermap.org/img/w/" + this.state.icon + ".png"
-     //Construct the image source URL from the name of the icon
 
     return (
       <div className="App">
@@ -74,22 +77,25 @@ class App extends Component {
           <h1 className="App-title">Weather Man</h1>
         </header>
 
-            <div className="weatherdata">
-              City: {this.state.city}<br/>
-              Temperature: {this.state.temperature} °C<br/>
-              Weather: {this.state.weather}<br/>
-              <img src={url} alt='icon'/><br/>
-            </div>
+        <div className="weatherdata">
+          City: {this.state.city}<br/>
+          Temperature: {this.state.temperature} °C<br/>
+          Weather: {this.state.weather}<br/>
 
-            <div className="buttons">
-              <input type='text' onChange={this.inputChanged} value={this.state.userInput} placeholder='city'></input>
-              <button onClick={this.getWeather}>Get weather</button>
-              <br/>
-              <button onClick={this.saveCity}>Save city</button>
-              <button onClick={this.clear}>Clear saved</button>
-            </div>
+          <img src={this.state.icon} alt='icon'/><br/>
+        </div>
 
-            <div className="allSaved">{itemRows}</div>
+        <div className="buttons">
+          <input type='text' onChange={this.inputChanged} value={this.state.userInput} placeholder='city'></input>
+          <button onClick={this.getWeather}>Get weather</button>
+          <br/>
+          <button onClick={this.saveCity}>Save city</button>
+          <button onClick={this.clear}>Clear saved</button>
+        </div>
+
+        <div className="allSaved">
+          {cityDivs}
+        </div>
 
       </div>
     );
